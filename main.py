dcc0f57780e85101684ea3397deb1392355a3576
Juan Zambrano <108336218+JuanZam21@users.noreply.github.com>
2023-04-10 01:33:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,73 @@
-from bs4 import BeautifulSoup, Comment
-from mtranslate import translate
-import lxml
-import concurrent.futures
+# Imports and logo
+import random
+from replit import clear 
+from art import logo
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# read html file locally
-with open("C:/Users/karen/OneDrive/Documentos/AI/ClassCentral_Hindi/report.html", encoding="utf-8") as fp:
-  response = fp.read()
+# FUCTIONS
+# Cards picker 
+def dealCards(cards):
+    return random.choices(cards, k=2)
 
-# parse the HTML
-soup = BeautifulSoup(response, "html.parser")
+# Calculate score
+def calculateScore(cards):
+    score = sum(cards)
+    if score == 21 and len(cards) == 2:
+        return 0
+    if score > 21 and 11 in cards:
+       cards.remove(11)
+       cards.append(1)
+    return score
 
-# Remove all comments
-for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
-    comment.extract()
-
-# find all text elements in the HTML
-text_elements = soup.find_all(string=True)
-
-# define a function to translate a text element
-def translate_element(element):
-    if element.parent.name in ['script', 'style']:
-        return str(element)
-    try:
-        translated_text = translate(element, 'hi')
-        return translated_text
-    except:
-        return str(element)
+# Compare fuction
+def compare(userScore, computerScore):
+  if userScore > 21 and computerScore > 21:
+    return "You went over. You lose 😤"
+  if userScore == computerScore:
+    return "Draw"
+  elif computerScore == 0:
+    return "Lose, opponent has blackjack"
+  elif userScore == 0:
+    return "Win with a blackjack"
+  elif userScore > 21:
+    return "You went over. You lose"
+  elif computerScore > 21:
+    return "Opponent went over. You win"
+  elif userScore > computerScore:
+    return "You win"
+  else:
+    return "Computer wins"
     
-# use multi-threading to translate the text elements
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = executor.map(translate_element, text_elements)
+# Game fuction
+def blackjack():
+  print(logo)
+  endGame = False
+  userCards = dealCards(cards)
+  computerCards = dealCards(cards)
+  
+  while not endGame: 
+    userScore = calculateScore(userCards)
+    computerScore = calculateScore(computerCards)
+        
+  # Game start  
+    print(f"{userCards} Your current score is: {userScore}")
+    print(f"Computer first card: {computerCards[0]}")
+    if userScore == 0 or computerScore == 0 or userScore > 21:
+      endGame = True
+    else:
+      if input("Type 'y' to get another card, type 'n' to end game: ").lower() == "y":
+        userCards.append(random.choices(cards, k=1)[0])
+      else:
+        endGame = True
+  while computerScore != 0 and computerScore < 17:
+    computerCards.append(random.choices(cards, k=1)[0])
+    computerScore = calculateScore(computerCards)
+  
+  print(f" Your final hand: {userCards}, final score {userScore}")
+  print(f" Computer's final hand: {computerCards}, final score {computerScore}")
+  print(compare(userScore, computerScore))
 
-# update the HTML with the translated text
-for element, translated_text in zip(text_elements, results):
-    element.replace_with(translated_text)
-
-# save the modified HTML
-with open("C:/Users/karen/OneDrive/Documentos/AI/ClassCentral_Hindi/report.html", "w", encoding="utf-8") as file:
-  file.write(soup.prettify())
+# Start
+while input("Do you want to play blackjack game, type 'y' or 'n': ").lower() == "y":
+  clear()
+  blackjack()
